fc8050096c402b45fdabd8e30f592b97f284c7a9
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-12-01 15:55:29 +0530

    modifying CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const Container = styled.div`
+height:100%;    
+overflow-y:scroll;
+`
 
 const CreatePost = () => {
-  return (
-    <div>CreatePost</div>
-  )
+    return (
+        <div>CreatePost</div>
+    )
 }
 
 export default CreatePost
