a26e4fabd3e523c7606332439653c294ca73434f
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-12-02 14:40:05 +0530

    modifying Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,26 @@
-import { AddRounded } from '@mui/icons-material';
-import Button from './button';
+import { AddRounded } from '@mui/icons-material'
+import Button from './button'
 import React from 'react'
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Container = styled.div`
     flex:1;
     background:${({ theme }) => theme.navbar};
     color:${({ theme }) => theme.text_primary};
-    font-weight:bold;
-    font-size:22px;
-    padding : 14px 50px;
+    fontWeight:bold;
+    fontsize:22px;
+    padding:14px 50px;
     display:flex;
     justify-content:space-between;
-    align-items:center;
-    box-shadow: 0 0 10px 0 rgba(0,0,0,0.15);
-`;
+    align-items:center; 
+    box-shadow: 0 0px 10px 0 rgba(0, 0, 0, 0.15);
+`
 
 const Navbar = () => {
     return (
         <Container>
             GenAI
-            <Button text="Create new Post" leftIcon={<AddRounded style={{ fontSize: "18px" }} />} />
+            <Button text="Create new post" leftIcon={<AddRounded fontSize='18px' />}></Button>
         </Container>
     )
 }
